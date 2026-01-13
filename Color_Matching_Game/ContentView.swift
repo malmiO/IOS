4e188b18979878b5419c8b77c94847e7048f907c
Malmi <malmioshadhi2001@gmail.com>
2026-01-13 14:03:58 +0530

    first commit

--- Color_Matching_Game/ContentView.swift
+++ Color_Matching_Game/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Let's start creating color matching game")
         }
         .padding()
     }
