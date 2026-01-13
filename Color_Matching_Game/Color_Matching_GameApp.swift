1eb35aed522d7078ccecb1c1514a73e5716befbd
Malmi <malmioshadhi2001@gmail.com>
2026-01-13 13:58:24 +0530

    Initial Commit

---
+++ Color_Matching_Game/Color_Matching_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Matching_GameApp.swift
+//  Color_Matching_Game
+//
+//  Created by COBSCCOMP24.2P-074 on 2026-01-13.
+//
+
+import SwiftUI
+
+@main
+struct Color_Matching_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color_Matching_Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Color_Matching_Game
+//
+//  Created by COBSCCOMP24.2P-074 on 2026-01-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
